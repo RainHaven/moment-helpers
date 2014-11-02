72982e9276e1969d35f3573b78aae13a95e13f30
Scott McPherson <scott_fl@me.com>
2014-11-02 09:11:37 -0500

    Add rainhaven prefix to tinytest

--- package.js
+++ package.js
@@ -15,6 +15,6 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('moment-helpers');
+  api.use('rainhaven:moment-helpers');
   api.addFiles('moment-helpers-tests.js');
 });
